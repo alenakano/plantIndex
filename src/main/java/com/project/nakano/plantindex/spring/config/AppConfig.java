f690e7feb2c52a39920801aad2790f93a819ed7a
Alexandre Nakano Marques <alexandre.nakano1@ibm.com>
2020-12-11 09:44:06 -0300

    Configs and properties database.

---
+++ src/main/java/com/project/nakano/plantindex/spring/config/AppConfig.java
@@ -0,0 +1,35 @@
+package com.project.nakano.plantindex.spring.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.project.nakano.plantindex.spring")
+@PropertySource("classpath:database.properties")
+public class AppConfig {
+
+	@Autowired
+	Environment environment;
+
+	private final String URL = "database.plantindex.url";
+	private final String USER = "database.plantindex.user";
+	private final String DRIVER = "database.plantindex.driver";
+	private final String PASSWORD = "database.plantindex.password";
+
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setUrl(environment.getProperty(URL));
+		driverManagerDataSource.setUsername(environment.getProperty(USER));
+		driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
+		driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+		return driverManagerDataSource;
+	}
+}
