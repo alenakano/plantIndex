0d3ebdae50362d05a5802b337c23705427b2123e
Alexandre Nakano <alex.nakano@gmail.com>
2021-03-21 00:43:37 -0300

    Code Style.

--- src/main/java/com/project/nakano/plantindex/jpa/model/TipoPropagacao.java
+++ src/main/java/com/project/nakano/plantindex/jpa/model/TipoPropagacao.java
@@ -1,19 +1,19 @@
 package com.project.nakano.plantindex.jpa.model;
 
 public enum TipoPropagacao {
-	ESTACA(1L),
-	TOUCEIRA(2L),
-	MUDA(3L),
-	SEMENTE(4L),
-	BULBO(5L);
-	
-	private final Long value;
-	
-	TipoPropagacao(Long value){
-		this.value = value;
-	}
-	
-	public Long getValue(){
-		return this.value;
-	}
+  ESTACA(1L),
+  TOUCEIRA(2L),
+  MUDA(3L),
+  SEMENTE(4L),
+  BULBO(5L);
+  
+  private final Long value;
+  
+  TipoPropagacao(Long value) {
+    this.value = value;
+  }
+  
+  public Long getValue() {
+    return this.value;
+  }
 }
