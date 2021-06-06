500fc0e2313ec11e7561751aa9c726a7fb86ca32
Alexandre Nakano <alex.nakano@gmail.com>
2021-06-06 11:37:12 -0300

    Category and search by name and category.

---
+++ src/main/java/com/project/nakano/plantindex/jpa/SearchController.java
@@ -0,0 +1,56 @@
+package com.project.nakano.plantindex.jpa;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.nakano.plantindex.jpa.model.Categoria;
+import com.project.nakano.plantindex.jpa.model.SearchResult;
+
+@RestController
+public class SearchController {
+
+  @Autowired
+  private PlantRepository plantRepository;
+  
+  @Autowired
+  private CategoriaRepository categoriaRepository;
+  
+  /** Resposta de busca pelo nome da planta e/ou categoria.
+  *
+  * @param name
+  * @param cat
+  *
+  * @return String
+  *
+  */
+  @GetMapping("/searchplants")
+  public List<SearchResult> findPlants(
+      @RequestParam(value = "name", defaultValue = "") String name, 
+      @RequestParam(value = "category", defaultValue = "") String cat) {
+    if (cat.isEmpty()) {
+      return plantRepository.searchByNome(name);
+    } 
+    if (name.isEmpty()) {
+      return plantRepository.searchByCategoria(cat);
+    }
+    return plantRepository.searchByNomeAndCategoria(name, cat);
+  }
+
+  /** Resposta de busca pelo nome da planta e/ou categoria.
+  *
+  * @param name
+  * @param cat
+  *
+  * @return String
+  *
+  */
+  @GetMapping("/categories")
+  public List<Categoria> fetchCategories() {
+    return (List<Categoria>) categoriaRepository.findAll();
+  }
+  
+}
